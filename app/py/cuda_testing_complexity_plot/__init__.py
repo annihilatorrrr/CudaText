a1ee9aa5f485ea0cac5438f7710aaa1ea466db15
Alexey T <support@uvviewsoft.com>
2026-09-13 14:14:29 +0300

    fix const in testing-plg

--- app/py/cuda_testing_complexity_plot/__init__.py
+++ app/py/cuda_testing_complexity_plot/__init__.py
@@ -124,7 +124,7 @@ class Command:
             ed.cmd(cmd_FileClose)
             return result
             
-        plot(test(300), offsetx=300)
+        plot(test(600), offsetx=300)
     
     def run_replace_lines(self):
         def test(n):
